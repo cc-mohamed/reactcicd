b0016bb955caf616f42e440199b2e9bd6c1152f2
Mohamed Kalanther <mohamedkalanther@6storage.com>
2023-02-24 12:45:13 +0530

    new design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save and Debloy to CI/CD then reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100" style={{ backgroundColor: '#eee' }}>
+      <MDBContainer className="container py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol md="12" xl="4">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="text-center">
+                <div className="mt-3 mb-4">
+                  <MDBCardImage src="https://static.wixstatic.com/media/497ce6_1e433e2bfb00450cb069aa75ed788370~mv2.png/v1/fill/w_198,h_194,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Halli%20Ragnarsson.png"
+                    className="rounded-circle" fluid style={{ width: '100px' }} />
+                </div>
+                <MDBTypography tag="h4">Mohamed Kalanther</MDBTypography>
+                <MDBCardText className="text-muted mb-4">
+                  @Mobile App Developer <span className="mx-2">|</span> <a href="https://www.caddycode.com/">caddycode.com</a>
+                </MDBCardText>
+                <div className="mb-4 pb-2">
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="facebook" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating className="mx-1">
+                    <MDBIcon fab icon="twitter" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="skype" size="lg" />
+                  </MDBBtn>
+                </div>
+                <MDBBtn rounded size="lg">
+                  Message now
+                </MDBBtn>
+                {/* <div className="d-flex justify-content-between text-center mt-5 mb-2">
+                  <div>
+                    <MDBCardText className="mb-1 h5">0</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Wallets Balance</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">0</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">0</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Total Transactions</MDBCardText>
+                  </div>
+                </div> */}
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </div>
   );
 }
-
-export default App;
